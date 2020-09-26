e3b37c92cd6599865d04921213618bd3411fd35f
Sameersaini12 <sameersaini35806@gmail.com>
2020-09-26 16:42:22 +0530

    Updates

---
+++ precache-manifest.5dcb852cc16593e43ab9b8cc68aaa585.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d0731a31498b7bba0720f6252c30e5",
+    "url": "/cricket-score/index.html"
+  },
+  {
+    "revision": "ed2ec2d41f92bc47a46d",
+    "url": "/cricket-score/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "02b7d89fca81d4685253",
+    "url": "/cricket-score/static/js/2.ae1ed042.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cricket-score/static/js/2.ae1ed042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2ec2d41f92bc47a46d",
+    "url": "/cricket-score/static/js/main.c808cf2a.chunk.js"
+  },
+  {
+    "revision": "346fabfdeea815aeb543",
+    "url": "/cricket-score/static/js/runtime-main.12ae44a7.js"
+  }
+]);
